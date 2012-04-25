eb5f0c6731b60246096f7bb475752710fb4c4872
Galuh Utama <galuh.utama@gwutama.de>
2012-04-25 23:15:16 +0200

    Summary function

--- textfinder.py
+++ textfinder.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python
 
 """
-A very smart script to iterate all files in a directory,
-then show every file which has a specific string in it with the line numbers.
+A very smart script to iterate all files in a directory, then show every file 
+which has a specific string in it with the line numbers.
 
 2012 Galuh Utama <galuh.utama@gwutama.de>
 Licensed under GPLV3
@@ -27,19 +27,50 @@ class Colors:
     FAIL = '\033[91m'
     ENDC = '\033[0m'
 
-    def disable(self):
-        self.HEADER = ''
-        self.OKBLUE = ''
-        self.OKGREEN = ''
-        self.WARNING = ''
-        self.FAIL = ''
-        self.ENDC = ''
+
+def textfinder(directory, wildcard, regex, interactive):
+    """
+    Finds a string inside files in a directory and pretty print them.
+    """
+    num_files = 0
+    num_dirs = 0
+    num_matches_strings = 0
+    num_matches_files = 0
+
+    # iterate every file in this directory
+    for file in os.listdir(directory):
+        # call textfinder again if this is a directory, 
+        # otherwise,find matches if it matches the wildcard.
+        file = directory + '/' + file
+
+        if os.path.isdir(file):
+            subdir = file
+            textfinder(subdir, wildcard, regex, interactive)
+            num_dirs = num_dirs + 1
+        elif fnmatch.fnmatch(file, wildcard) and os.path.isfile(file):
+            matches = search_string(file, regex)
+            if matches:
+                print Colors.OKGREEN + '%s' % (file, )                
+                for match in matches:
+                    print match
+                print Colors.ENDC
+                if interactive:
+                    raw_input()
+                num_matches_strings = num_matches_strings + len(matches)
+                num_matches_files = num_matches_files + 1
+                num_files = num_files + 1
+        else:
+            num_files = num_files + 1
+
+    return dict(num_files=num_files, num_dirs=num_dirs, 
+        num_matches_files=num_matches_files, 
+        num_matches_strings=num_matches_strings)
 
 
 def search_string(filename, regex):
     """
-    Search something in a file based on regex.
-    Prints out the occurence with the line numbers.
+    Search something in a file based on regex. Prints out the occurence 
+    with the line numbers.
     """
     f = open(filename, 'r')
     matches = []
@@ -54,51 +85,39 @@ def search_string(filename, regex):
         i = i+1
         if prog.match(line):
             text = line.rstrip()
-            text = re.sub(regex, '\\1' + Colors.WARNING + '\\2' + Colors.ENDC + '\\3', text)
+            text = re.sub(regex, '\\1' + Colors.WARNING + '\\2' + Colors.ENDC 
+                    + '\\3', text)
             match = '%s%03d  %s%s' % (Colors.OKBLUE, i, Colors.ENDC, text,)
             matches.append(match)
     return matches
 
 
-def textfinder(directory, wildcard, regex, interactive):
+def summary(result):
     """
-    Finds a string inside files in a directory
-    and pretty print them.
+    Prints the summary
     """
-    # iterate every file in this directory
-    for file in os.listdir(directory):
-        # call textfinder again if this is a directory, 
-        # otherwise,find matches if it matches the wildcard.
-        file = directory + '/' + file
-        if os.path.isdir(file):
-            subdir = file
-            textfinder(subdir, wildcard, regex, interactive)
-        elif fnmatch.fnmatch(file, wildcard):
-            matches = search_string(file, regex)
-            if matches:
-                print Colors.OKGREEN + '%s' % (file, )
-                
-                for match in matches:
-                    print match
-
-                print Colors.ENDC
-
-                if interactive:
-                    raw_input()
+    print 'Summary'
+    print '-------'
+    print 'Documents iterated:\t%d' % (result['num_dirs'])
+    print 'Files iterated:\t\t%d' % (result['num_files'])
+    print 'Matched files:\t\t%d' % (result['num_matches_files'])
+    print 'Matched strings:\t%d' % (result['num_matches_strings'])
+    print
 
 
 def main():
     """
     The main function to run the whole script.
     """
-    parser = argparse.ArgumentParser(description="""A smart script to iterate
-                all files recursively in a directory, then show every file
+    parser = argparse.ArgumentParser(description="""A smart script to iterate 
+                all files recursively  in a directory, then show every file
                 which has a specific string in it.""")
 
     parser.add_argument('directory', nargs=1, help='Base directory')
     parser.add_argument('wildcard', nargs=1, help='Unix style file name match')
     parser.add_argument('regex', nargs=1, help='Regular expression')    
-    parser.add_argument('--interactive', '-i', help='Stop on every match', action='store_true')
+    parser.add_argument('--interactive', '-i', help='Stop on every match', 
+        action='store_true')
 
     args = parser.parse_args()
 
@@ -108,10 +127,11 @@ def main():
         help()        
         sys.exit(2)
 
-    # At this point we will assume that regex and wildcard are always right anyway
     # Run the finder function now.
     try:
-        textfinder(args.directory[0], args.wildcard[0], args.regex[0], args.interactive)
+        res = textfinder(args.directory[0], args.wildcard[0], args.regex[0], 
+                args.interactive)
+        summary(res)
     except KeyboardInterrupt:
         pass
 
